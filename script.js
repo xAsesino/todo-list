c16eeb3298dbec5ddc6135df91abf6e50b749657
xAsesino <a.bayhoca@outlook.com>
2021-11-12 14:30:11 +0100

    testerrrr

--- script.js
+++ script.js
@@ -8,14 +8,13 @@ const listacquis = document.querySelector(".acquis");
 const myInputli = document.getElementById("myInput");
 const addtolist = document.querySelector(".btnadd");
 
-
 JSONtab();
 
 addtolist.addEventListener('click', () => {
 
     tab.push(myInputli.value);
     tab1.push(myInputli.value);
-    
+
     listchild.innerHTML += `
 
             <li class="test">${myInputli.value} 
@@ -25,11 +24,11 @@ addtolist.addEventListener('click', () => {
 
         `
     localStorage.setItem('myInputs', JSON.stringify(tab));
+    localStorage.setItem('MyGoods', JSON.stringify(tab1));
 
     myInputli.value = "";
 
     deltamere();
-    itsgood();
 
 })
 
@@ -56,22 +55,22 @@ function JSONtab() {
 
     }
 
-    if (localStorage.getItem('MyGoods', tab1) != null) {
+    // if (localStorage.getItem('MyGoods', tab1) != null) {
 
-        tab1 = JSON.parse(localStorage.getItem('MyGoods', tab1));
+    //     tab1 = JSON.parse(localStorage.getItem('MyGoods', tab1));
 
-        for (let i = 0; i < tab1.length; i++) {
+    //     for (let i = 0; i < tab1.length; i++) {
 
-            listacquis.innerHTML += `
-            <li class="test">${tab1[i]}<button class="btndel" data-id="${tab1[i]}">Supprimer</button></li>
-            
-        
-            `
-        }
+    //         listacquis.innerHTML += `
+    //         <li class="test">${tab1[i]}<button class="btndel" data-id="${tab1[i]}">Supprimer</button></li>
 
-        itsgood();
 
-    }
+    //         `
+    //     }
+
+    //     itsgood();
+
+    // }
 
 }
 
@@ -104,6 +103,7 @@ function deltamere() {
         })
 
     }
+    itsgood();
 
 }
 
@@ -114,71 +114,24 @@ function itsgood() {
     for (const alllister of lister) {
 
         const btnacquis = alllister.querySelector('.acquiss');
-        let tag1 = btnacquis.getAttribute("data-id");
 
         btnacquis.addEventListener('click', () => {
-            
-            // tab1.push(myInputli.value);
-            
+
             for (let i = 0; i < tab1.length; i++) {
 
                 listacquis.innerHTML += `
 
-                    <li class="test">${tab1[i]}<button class="btndel" data-id="${tab1[i]}">Supprimer</button></li>
+                    <li class="tester">${tab1[i]}<button class="btndel">Supprimer</button></li>
                 
                 `
-            
-            }
 
-            for (let i = 0; i < tab.length; i++) {
-                
-                if (tag1 == tab[i]) {
-                    
-                    indice = i;
-                    
-                }
-                
+                tab.splice(indice, 1);
+                listchild.removeChild(alllister);
+                localStorage.setItem('myInputs', JSON.stringify(tab));
+
             }
-            
-            tab.splice(indice, 1);
-            listchild.removeChild(alllister);
-            localStorage.setItem('myInputs', JSON.stringify(tab));
-            
         })
         localStorage.setItem('MyGoods', JSON.stringify(tab1));
     }
 }
 
-
-
-
-// const btndell = document.getElementsByClassName("btndel");
-// const lister = document.querySelector(".test");
-
-// for (let i = 0; i < btndell.length; i++) {
-
-//     btndell[i].addEventListener('click', () => {
-
-//         let tag = btndell[i].getAttribute("data-id");
-
-//         if (tag == myInputli.value || tag == tab[i]) {
-
-//             if (indice == tab[i]) {
-
-//                 indice = i;
-
-//             }
-
-//             console.log(tag);
-
-                // tab.splice(indice, 1);
-//             // listchild.removeChild(lister);
-//             
-
-//             // localStorage.setItem('myInputs', JSON.stringify(tab));
-
-//         }
-
-//     })
-
-// }
